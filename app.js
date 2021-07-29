7033f2d2448e2fabda824b14b27bade51a369cb9
marcodm-1 <marcodiazmoore@gmail.com>
2021-07-29 15:53:38 -0400

    Added serverside query detection

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ url.parse("?first=hello&second=test")
 
 const server = http.createServer(function(request, response){
     if(request.url.split('?')[0] == "/SetKey"){ // Determine if the request is on our API url
-        const queryObject = url.parse(request.url).query;
-        console.log("setkey");
+        const queryObject = url.parse(request.url);
+        let contentType = "text/html";
         response.setHeader("Access-Control-Allow-Origin", '*');
-        response.setHeader("Content-Type", "application/json")
-        response.write("test");
+
+        if("account" in queryObject && "key" in queryObject){
+            console.log("Request received with account name: ", queryObject.account, " and the key being ", queryObject.key);
+            response.setHeader("Content-Type","application/json");
+            response.write(JSON.stringify(queryObject));
+        }else{
+            response.setHeader("Content-Type", "text/html");
+            response.write("A request was received with invalid querie(s) attached.");
+        }
         response.end();
     }else{ // Otherwise send a generic response back saying that the API doesn't exist
         console.log(`A request was sent at the different URL: ${request.url}`);
