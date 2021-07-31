e28ebc38251e7c4df0857fda426c5b11a43387d6
marcodm-1 <marcodiazmoore@gmail.com>
2021-07-31 15:14:06 -0400

    Created table functionality through parsing and processing of returned API data.

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 // Client side script that handles the index page.
 
-var accountName = document.getElementById("accountName");
-var whitelistKey = document.getElementById("whitelistKey");
-var available = document.getElementById("available");
-var set = document.getElementById("set");
+const accountName = document.getElementById("accountName");
+const whitelistKey = document.getElementById("whitelistKey");
+const whitelistTable = document.getElementById("whitelistTable");
 
+function createWhitelistEntry(key, name){
+    // Creates tables rows for each of the whitelists
+    const newTableRow = document.createElement("tr");
+    whitelistTable.appendChild(newTableRow);
+    var newWhitelistBox = document.createElement("td");
+    newWhitelistBox.innerText = key;
+    newWhitelistBox.setAttribute("style", "background-color: #e0e0e0;")
+    var newAccountBox = document.createElement("td");
+    newAccountBox.innerText = name;
+    newAccountBox.setAttribute("style", "background-color: #e0e0e0;")
+    whitelistTable.appendChild(newWhitelistBox);
+    whitelistTable.appendChild(newAccountBox);
+}
 
-function createAnchorElement(parent, text){
-    const newElement = document.createElement("a");
-    newElement.innerText = text;
-    parent.appendChild(newElement); // Put our new element into the intended parent element
+function clearPreviousWhitelistEntries(){
+    // Clears the whitelist table to allow for the new data to populate it.
+    whitelistTable.innerHTML = "";
+    var newTableRow = document.createElement("tr");
+    var newTableHeaderKey = document.createElement("th");
+    newTableHeaderKey.innerText = "Whitelist Key";
+    newTableRow.appendChild(newTableHeaderKey);
+    var newTableHeaderWhitelist = document.createElement("th");
+    newTableHeaderWhitelist.innerHTML = "Whitelist Owner";
+    newTableRow.appendChild(newTableHeaderWhitelist);
+    whitelistTable.appendChild(newTableRow);
 }
 
 async function apiRequest(account, key){
-    let requestURL = `http://localhost:3000/SetKey?account=${account}&key=${key}`;
+    let requestURL = `http://localhost:3000/SetKey?account=${String(account)}&key=${String(key)}`;
     let request = await fetch(requestURL);
     let requestJSON = await request.json();
     return requestJSON
@@ -22,7 +41,11 @@ async function apiRequest(account, key){
 function formSubmit(){
     if(accountName.value != "" & whitelistKey.value != ""){ // Don't bother wasting requests when there is no input        
         apiRequest(accountName.value, whitelistKey.value).then(function(result){
-            console.log(result);
+            clearPreviousWhitelistEntries();
+            for(let i = 0; i < result.length; i++){
+                // Iterate over the returned array and display each entry into the table.
+                createWhitelistEntry(result[i].key, result[i].account)
+            }
         }).catch(function(err){
             console.log("An error occured when trying to fetch the results from the server's API.");
         })
